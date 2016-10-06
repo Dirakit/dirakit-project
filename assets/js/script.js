d4553bd710b4cef079204ac260ab15929af3a7d7
Sendy Aditya Suryana <sendzation@gmail.com>
2016-10-06 17:24:06 +0700

    fix home

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* Load JS on document ready */
+$(document).ready(function () {
+    /* Semantic UI Initialization */
+    $('.tabbing.menu .item').tab();
+    $('.special.card .image').dimmer({on: 'hover'});
+    /* End of Semantic UI Initialization */
+});

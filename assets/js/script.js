3a38cf8f77f3c3f8208fd77f5cbc90659affcc15
Sendy Aditya Suryana <sendzation@gmail.com>
2016-10-13 22:23:11 +0700

    add challenge-3 page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,6 @@ $(document).ready(function () {
     /* Semantic UI Initialization */
     $('.tabbing.menu .item').tab();
     $('.special.card .image').dimmer({on: 'hover'});
+    $('select.dropdown').dropdown();
     /* End of Semantic UI Initialization */
 });

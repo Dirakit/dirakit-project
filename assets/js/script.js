3ed27fb91e8f4f0a883e93503ce63101be729184
Sendy Aditya Suryana <sendzation@gmail.com>
2016-10-16 21:54:12 +0700

    new page project overview

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,8 @@ $(document).ready(function () {
     $('.tabbing.menu .item').tab();
     $('.special.card .image').dimmer({on: 'hover'});
     $('select.dropdown').dropdown({forceSelection:false});
+    $('.ui.small.images > .image').click(function () {
+        $('.ui.modal').modal('show');
+    });
     /* End of Semantic UI Initialization */
 });

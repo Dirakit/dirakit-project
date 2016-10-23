9a05052d3df485c9ff69fb0d71be34e495463c2a
Sendy Aditya Suryana <sendzation@gmail.com>
2016-10-23 14:52:25 +0530

    [Issue #8] Add new step on challenge 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     /* Semantic UI Initialization */
     $('.tabbing.menu .item').tab();
     $('.special.card .image').dimmer({on: 'hover'});
-    $('select.dropdown').dropdown({forceSelection:false});
+    $('select.dropdown').dropdown({forceSelection: false});
     $('.ui.small.images > .image').click(function () {
         $('.ui.modal').modal('show');
     });
@@ -17,4 +17,14 @@ $(document).ready(function () {
         duration: 1000
     });
     /* End of Semantic UI Initialization */
+    $('.add-step').click(function () {
+        addStep();
+    });
 });
+
+function addStep() {
+    var stepHtml = $('.custom-step');
+    var step = stepHtml.html();
+    step = step += step;
+    stepHtml.html(step);
+}

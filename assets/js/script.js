f41dcc434fb09e6a8aff4f9f86686443c6793975
Sendy Aditya Suryana <sendzation@gmail.com>
2016-11-13 02:20:56 +0700

    [Issue #36] Text editor for challenge page 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,10 @@ $(document).ready(function () {
     /* End of Semantic UI Initialization */
 });
 
+function initFroala() {
+    $(this).froalaEditor();
+}
+
 $('.add-step').click(function () {
     addStep();
 });
@@ -43,123 +47,116 @@ $('.add-step').click(function () {
 var counter = 1;
 function addStep() {
     var stepHtml = $('.custom-step');
-    var steps = "<div class='step-" + counter + "'>" +
-        "<div class='inline fields'>" +
-        "<div class='four wide field'>" +
-        "Intro" +
-        "</div>" +
-        "<div class='eight wide field'>" +
-        "<input type='text' placeholder='Judul Intro'>" +
-        "</div>" +
-        "<div class='four wide field'>" +
-        "<button class='ui button fluid'><i class='upload icon'></i>Upload Gambar</button>" +
-        "</div>" +
-        "</div>" +
-        "<div class='inline fields'>" +
-        "<div class='four wide field'>" +
+    var steps = "<div class='challenge-steps-container step-" + counter + "'>" +
         "Langkah &nbsp; <span class='step-counter'>" + counter + "</span>" +
-        "</div>" +
-        "<div class='eight wide field'>" +
-        "<textarea name='step' id='step'></textarea>" +
-        "</div>" +
-        "<div class='four wide field'>" +
-        "<button class='ui button fluid'><i class='upload icon'></i>Upload Gambar</button>" +
-        "</div>" +
-        "</div>" +
+            "<div class='sixteen wide field'>" +
+                "<input type='text' placeholder='Judul Intro'>" +
+            "</div>" +
+            "<div class='sixteen wide field'>" +
+                "<button class='ui button fluid'><i class='upload icon'></i>Upload Gambar</button>" +
+            "</div>" +
+            "<div class='sixteen wide field'>" +
+                "<textarea name='step' id='step-"+counter+"' class='challenge-steps' rows='30'></textarea>" +
+            "</div>" +
         "</div>";
-    counter++;
 
     var step = stepHtml.html();
     step = step += steps;
     stepHtml.html(step);
+
+    var textareaId = '#step-' + counter;
+    console.log(textareaId);
+    $(textareaId).froalaEditor();
+    counter++;
+
 }
-var infiniteContents = '<div class="column">'+
-    '<div class="ui fluid special card">'+
-    '<div class="blurring dimmable image">'+
-    '<div class="ui dimmer">'+
-    '<div class="content">'+
-    '<a class="center" href="#">'+
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!'+
-'</a>'+
-'</div>'+
-'</div>'+
-'<img src="assets/images/13.png" alt="13">'+
-    '</div>'+
-    '<div class="content">'+
-    '<a href="#" class="header">Lorem Ipsum</a>'+
-'<div class="meta">'+
-    '<a href="#"><i class="folder icon"></i>Showcase</a>'+
-    '<a href="#"><i class="user icon"></i>Admin</a>'+
-    '<a href="#"><i class="unhide icon"></i>2200</a>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
-    '<div class="column">'+
-    '<div class="ui fluid special card">'+
-    '<div class="blurring dimmable image">'+
-    '<div class="ui dimmer">'+
-    '<div class="content">'+
-    '<a class="center" href="#">'+
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!'+
-'</a>'+
-'</div>'+
-'</div>'+
-'<img src="assets/images/13.png" alt="13">'+
-    '</div>'+
-    '<div class="content">'+
-    '<a href="#" class="header">Lorem Ipsum</a>'+
-'<div class="meta">'+
-    '<a href="#"><i class="folder icon"></i>Showcase</a>'+
-    '<a href="#"><i class="user icon"></i>Admin</a>'+
-    '<a href="#"><i class="unhide icon"></i>2200</a>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
-    '<div class="column">'+
-    '<div class="ui fluid special card">'+
-    '<div class="blurring dimmable image">'+
-    '<div class="ui dimmer">'+
-    '<div class="content">'+
-    '<a class="center" href="#">'+
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!'+
-'</a>'+
-'</div>'+
-'</div>'+
-'<img src="assets/images/13.png" alt="13">'+
-    '</div>'+
-    '<div class="content">'+
-    '<a href="#" class="header">Lorem Ipsum</a>'+
-'<div class="meta">'+
-    '<a href="#"><i class="folder icon"></i>Showcase</a>'+
-    '<a href="#"><i class="user icon"></i>Admin</a>'+
-    '<a href="#"><i class="unhide icon"></i>2200</a>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
-    '<div class="column">'+
-    '<div class="ui fluid special card">'+
-    '<div class="blurring dimmable image">'+
-    '<div class="ui dimmer">'+
-    '<div class="content">'+
-    '<a class="center" href="#">'+
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!'+
-'</a>'+
-'</div>'+
-'</div>'+
-'<img src="assets/images/13.png" alt="13">'+
-    '</div>'+
-    '<div class="content">'+
-    '<a href="#" class="header">Lorem Ipsum</a>'+
-'<div class="meta">'+
-    '<a href="#"><i class="folder icon"></i>Showcase</a>'+
-    '<a href="#"><i class="user icon"></i>Admin</a>'+
-    '<a href="#"><i class="unhide icon"></i>2200</a>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
+var infiniteContents = '<div class="column">' +
+    '<div class="ui fluid special card">' +
+    '<div class="blurring dimmable image">' +
+    '<div class="ui dimmer">' +
+    '<div class="content">' +
+    '<a class="center" href="#">' +
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!' +
+    '</a>' +
+    '</div>' +
+    '</div>' +
+    '<img src="assets/images/13.png" alt="13">' +
+    '</div>' +
+    '<div class="content">' +
+    '<a href="#" class="header">Lorem Ipsum</a>' +
+    '<div class="meta">' +
+    '<a href="#"><i class="folder icon"></i>Showcase</a>' +
+    '<a href="#"><i class="user icon"></i>Admin</a>' +
+    '<a href="#"><i class="unhide icon"></i>2200</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '<div class="column">' +
+    '<div class="ui fluid special card">' +
+    '<div class="blurring dimmable image">' +
+    '<div class="ui dimmer">' +
+    '<div class="content">' +
+    '<a class="center" href="#">' +
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!' +
+    '</a>' +
+    '</div>' +
+    '</div>' +
+    '<img src="assets/images/13.png" alt="13">' +
+    '</div>' +
+    '<div class="content">' +
+    '<a href="#" class="header">Lorem Ipsum</a>' +
+    '<div class="meta">' +
+    '<a href="#"><i class="folder icon"></i>Showcase</a>' +
+    '<a href="#"><i class="user icon"></i>Admin</a>' +
+    '<a href="#"><i class="unhide icon"></i>2200</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '<div class="column">' +
+    '<div class="ui fluid special card">' +
+    '<div class="blurring dimmable image">' +
+    '<div class="ui dimmer">' +
+    '<div class="content">' +
+    '<a class="center" href="#">' +
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!' +
+    '</a>' +
+    '</div>' +
+    '</div>' +
+    '<img src="assets/images/13.png" alt="13">' +
+    '</div>' +
+    '<div class="content">' +
+    '<a href="#" class="header">Lorem Ipsum</a>' +
+    '<div class="meta">' +
+    '<a href="#"><i class="folder icon"></i>Showcase</a>' +
+    '<a href="#"><i class="user icon"></i>Admin</a>' +
+    '<a href="#"><i class="unhide icon"></i>2200</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '<div class="column">' +
+    '<div class="ui fluid special card">' +
+    '<div class="blurring dimmable image">' +
+    '<div class="ui dimmer">' +
+    '<div class="content">' +
+    '<a class="center" href="#">' +
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequuntur, cum dolor est ipsa ipsam ipsum labore mollitia neque nihil nisi numquam odio officiis placeat sed ut? Quas, reiciendis!' +
+    '</a>' +
+    '</div>' +
+    '</div>' +
+    '<img src="assets/images/13.png" alt="13">' +
+    '</div>' +
+    '<div class="content">' +
+    '<a href="#" class="header">Lorem Ipsum</a>' +
+    '<div class="meta">' +
+    '<a href="#"><i class="folder icon"></i>Showcase</a>' +
+    '<a href="#"><i class="user icon"></i>Admin</a>' +
+    '<a href="#"><i class="unhide icon"></i>2200</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
     '</div>';
 var count = 1;
 window.loadFakeContent = function () {
